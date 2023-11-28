22700386e0f1ad8e1813d2dac8dac4567cb8c5eb
Anuj Kashyap <anujs5761@gmail.com>
2023-11-28 23:01:06 +0530

    Changes and Labs file added

--- Javascript.js
+++ Javascript.js
@@ -13,3 +13,18 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load" , function(){
    loader.style.display = "none";
 })
+
+
+// Labs
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
