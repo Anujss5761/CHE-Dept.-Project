57502b8e4c45e4ce16608690b60983127b481ec7
Anuj Kashyap <anujs5761@gmail.com>
2024-02-08 23:22:01 +0530

    mm

--- Javascript.js
+++ Javascript.js
@@ -28,3 +28,25 @@ prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
 })
+
+
+// Confetti
+
+const confettiBtn = document.querySelector(".confetti-button");
+
+confettiBtn.addEventListener("Click",   ()=>{
+    const canvas = document.createElement("canvas");
+    const container = document.querySelector(".wrapper");
+
+    // set dimension for canvas
+    canvas.width = 600;
+    canvas.height = 600;
+
+    container.appendChild(canvas);
+
+    // create confetti inside the canvas
+    const confetti_btn = confetti.create(canvas);
+    
+    // remove the canvas after spreading confetti
+    confetti_btn.then(() => canvas.remove());
+});
